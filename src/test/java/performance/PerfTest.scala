54e7f0385bf85a87d696c40d05a9b32648faabe9
Lorblak <kvasquez@fi.uba.ar>
2021-07-16 20:23:00 -0300

    feat(PerfTest.scala): gatling performance test was added

---
+++ src/test/java/performance/PerfTest.scala
@@ -0,0 +1,40 @@
+package conduitApp.performance
+
+import com.intuit.karate.gatling.PreDef._
+import io.gatling.core.Predef._
+
+import scala.concurrent.duration.DurationInt
+
+// import conduitApp.performance.createTokens.CreateTokens
+
+class PerfTest extends Simulation {
+
+  //CreateTokens.createAccessTokens();
+
+  val protocol = karateProtocol(
+    "/api/articles/{articleId}" -> Nil
+  )
+
+  // protocol.nameResolver = (req, ctx) => req.getHeader("karate-name")
+
+  //val csvFeeder = csv("articles.csv").circular // use a comma separator
+
+  //val tokenFeeder = Iterator.continually(Map("token" -> CreateTokens.getNextToken))
+
+  val createArticle = scenario("Create and delete article")
+    //.feed(csvFeeder)
+    //.feed(tokenFeeder)
+    .exec(karateFeature("classpath:petswagger/features/pet-example-01.feature"))
+
+  setUp(
+    createArticle.inject(
+      atOnceUsers(1),
+      nothingFor(4 seconds),
+      constantUsersPerSec(1) during(3 seconds)
+      //constantUsersPerSec(2) during(10 seconds),
+      //rampUsersPerSec(2) to 10 during(20 seconds),
+      //nothingFor(5 seconds),
+      //constantUsersPerSec(1) during (10 seconds)
+    ).protocols(protocol)
+  )
+}
--- src/test/java/petswagger/features/user-example-01.feature
+++ src/test/java/petswagger/features/user-example-01.feature
@@ -3,8 +3,22 @@ Feature: User
   Background: Preconditions
     * def version = "v2"
     * def operation = "user"
+    * def id = 1
+    * def username = "kvasquez"
+    * def firstName = "Kevin"
+    * def lastName = "Vasquez"
+    * def email = "kvasquez@icbc.com.ar"
+    * def password = "qwerty"
+    * def phone = "011111111"
     * def requestBody = read('../request/user.json')
     * def responseBody = read('../response/user.json')
+    * set requestBody[0].id = id
+    * set requestBody[0].username = "kvasquez"
+    * set requestBody[0].firstName = firstName
+    * set requestBody[0].lastName = lastName
+    * set requestBody[0].email = email
+    * set requestBody[0].password = password
+    * set requestBody[0].phone = phone
 
   Scenario: Creates list of users with given input array
     Given url apiUrl + "/" + version + "/"
@@ -19,7 +33,6 @@ Feature: User
     And path operation + "/kvasquez"
     When method GET
     Then status 200
-    #And match response == responseBody
 
   Scenario: Logs user into the system
     * def login = "login?username=kvasquez&password=qwerty"
