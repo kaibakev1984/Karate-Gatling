44a696e6cd1aba783c8e9cf12475f506e73af3c9
Lorblak <kvasquez@fi.uba.ar>
2021-07-20 01:07:27 -0300

    feat: add-likes feature was added

--- src/test/java/performance/PerfTest.scala
+++ src/test/java/performance/PerfTest.scala
@@ -24,17 +24,17 @@ class PerfTest extends Simulation {
   val createArticle = scenario("Create and delete article")
     //.feed(csvFeeder)
     //.feed(tokenFeeder)
-    .exec(karateFeature("classpath:petswagger/features/pet-example-01.feature"))
+    .exec(karateFeature("classpath:conduit/features/new-article-1.feature"))
 
   setUp(
     createArticle.inject(
       atOnceUsers(1),
       nothingFor(4 seconds),
-      constantUsersPerSec(1) during(3 seconds)
-      //constantUsersPerSec(2) during(10 seconds),
-      //rampUsersPerSec(2) to 10 during(20 seconds),
-      //nothingFor(5 seconds),
-      //constantUsersPerSec(1) during (10 seconds)
+      //constantUsersPerSec(1) during(3 seconds)
+      constantUsersPerSec(2) during(10 seconds),
+      rampUsersPerSec(2) to 10 during(20 seconds),
+      nothingFor(5 seconds),
+      constantUsersPerSec(1) during (10 seconds)
     ).protocols(protocol)
   )
 }
