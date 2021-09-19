3d15749eddba3c7dbcb24d1affd0fb093948d51b
lorblak <kvasquez@fi.uba.ar>
2021-09-19 17:37:43 -0300

    fix: unused packages were deleted

--- src/test/java/karate-config.js
+++ src/test/java/karate-config.js
@@ -1,30 +1,12 @@
-function fn(){
+function fn() {
     var env = karate.env;
-    if(!env) {
-        env = "pet";
+    if (!env) {
+        env = "prod-pet";
     };
     karate.log("karate environment system property is: ", env);
-    var config = {
-        apiUrl: "https://conduit.productionready.io/api/",
-        petUrl: "https://petstore.swagger.io/"
-    };
-
-    if (env == 'dev') {
-        config.userEmail = 'karate@test.com'
-        config.userPassword = 'karate123'
+    var config = {};
+    if (env == 'prod-pet') {
+        config.apiUrl = "https://petstore.swagger.io/";
     }
-
-    if (env == 'qa') {
-        config.userEmail = 'karate2@test.com'
-        config.userPassword = 'Karate456'
-    }
-    if (env != "pet") {
-       // --- conduit config ---
-       var accessToken = karate.callSingle('classpath:utils/reusable_steps/create-token.feature', config).authToken;
-       karate.configure('headers', {Authorization: 'Token ' + accessToken});
-    }
-    // --- conduit config ---
-   // var accessToken = karate.callSingle('classpath:utils/reusable_steps/create-token.feature', config).authToken;
-   // karate.configure('headers', {Authorization: 'Token ' + accessToken});
     return config;
 }
